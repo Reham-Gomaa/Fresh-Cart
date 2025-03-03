5361201dee42534867a562e2b909d883cb076853
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-03-03 19:13:25 +0200

    add to wishlist icon and products component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,6 +12,7 @@ import { AuthService } from '../../core/services/authentication/auth.service';
 import { CartService } from '../../core/services/cart/cart.service';
 import { ToastrService } from 'ngx-toastr';
 import { WishlistService } from '../../core/services/wishlist/wishlist.service';
+import { IWishlist } from '../../core/interfaces/wishlist/iwishlist';
 
 @Component({
   selector: 'app-home',
@@ -28,7 +29,10 @@ export class HomeComponent implements OnInit , OnDestroy{
   productSub !: Subscription;
   addCartSubID !: Subscription;
   addListSubID !: Subscription;
-  added :boolean = false;
+  loggedUserSubID !: Subscription;
+  removeSubID !: Subscription;
+  homeWishlist !:IWishlist[] ;
+  wishlistIds: Set<string> = new Set();
 
   constructor(
     private _ProductService:ProductService , 
@@ -86,6 +90,13 @@ export class HomeComponent implements OnInit , OnDestroy{
   ngOnInit(): void {
     this._AuthService.userInfo
 
+    this.loggedUserSubID = this._WishlistService.getLoggedUserWishlist().subscribe({
+      next:(res)=>{
+        this.homeWishlist = res.data;
+        this.wishlistIds = new Set(res.data.map((item: IWishlist) => item._id));
+      }
+    })
+
     this.categorySub = this._CategoriesService.getAllCategories().subscribe({
       next:(res)=>{
         this.categoryData = res.data;
@@ -108,14 +119,26 @@ export class HomeComponent implements OnInit , OnDestroy{
     })
   }
 
+  isInWishlist(productId: string): boolean {
+    return this.wishlistIds.has(productId);
+  }
+
   addToWishlist(p_id:string){
-    this.addListSubID = this._WishlistService.addProductToWishlist(p_id).subscribe({
-      next:(res)=>{
-        this.added = true;
-        this._ToastrService.success(res.message , 'FreshCart')
-      }
-    })
-    this.added = false;
+    if (this.isInWishlist(p_id)) {
+      this.removeSubID = this._WishlistService.removeProductFromWishlist(p_id).subscribe({
+        next: () => {
+          this.wishlistIds.delete(p_id);
+          this._ToastrService.warning('Removed from Wishlist', 'FreshCart');
+        }
+      });
+    } else {
+      this.addListSubID = this._WishlistService.addProductToWishlist(p_id).subscribe({
+        next: (res) => {
+          this.wishlistIds.add(p_id);
+          this._ToastrService.success('Added to Wishlist', 'FreshCart');
+        }
+      });
+    }
   }
 
   ngOnDestroy(): void {
@@ -123,5 +146,7 @@ export class HomeComponent implements OnInit , OnDestroy{
     this.productSub?.unsubscribe();
     this.addCartSubID?.unsubscribe();
     this.addListSubID?.unsubscribe();
+    this.loggedUserSubID?.unsubscribe();
+    this.removeSubID?.unsubscribe();
   }
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { ICategory } from '../../core/interfaces/categories/icategory';
+import { IProduct } from '../../core/interfaces/product/iproduct';
+import { IWishlist } from '../../core/interfaces/wishlist/iwishlist';
+import { AuthService } from '../../core/services/authentication/auth.service';
+import { CartService } from '../../core/services/cart/cart.service';
+import { CategoriesService } from '../../core/services/categories/categories.service';
+import { ProductService } from '../../core/services/products/product.service';
+import { WishlistService } from '../../core/services/wishlist/wishlist.service';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { SearchPipe } from '../../shared/pipes/search/search.pipe';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ SearchPipe , FormsModule , RouterLink ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
-
+export class ProductsComponent implements OnInit , OnDestroy{
+  
+    searchValue :string = '';
+    categoryData !: ICategory[];
+    productData !: IProduct[];
+    categorySub !: Subscription;
+    productSub !: Subscription;
+    addCartSubID !: Subscription;
+    addListSubID !: Subscription;
+    loggedUserSubID !: Subscription;
+    removeSubID !: Subscription;
+    homeWishlist !:IWishlist[] ;
+    wishlistIds: Set<string> = new Set();
+  
+    constructor(
+      private _ProductService:ProductService , 
+      private _CategoriesService: CategoriesService , 
+      private _AuthService : AuthService ,
+      private _CartService : CartService,
+      private _ToastrService : ToastrService,
+      private _WishlistService : WishlistService
+    ){};
+  
+    ngOnInit(): void {
+      this._AuthService.userInfo
+  
+      this.loggedUserSubID = this._WishlistService.getLoggedUserWishlist().subscribe({
+        next:(res)=>{
+          this.homeWishlist = res.data;
+          this.wishlistIds = new Set(res.data.map((item: IWishlist) => item._id));
+        }
+      })
+  
+      this.categorySub = this._CategoriesService.getAllCategories().subscribe({
+        next:(res)=>{
+          this.categoryData = res.data;
+        }
+      });
+  
+      this.productSub = this._ProductService.getAllProducts().subscribe({
+        next:(res)=>{
+          this.productData = res.data;
+        }
+      });
+    }
+  
+    addToCart(p_id:string){
+      this.addCartSubID = this._CartService.AddProductToCart(p_id).subscribe({
+        next: (res)=> {
+          this._CartService.numOfCartItems.next(res.numOfCartItems);  
+          this._ToastrService.success( res.message , 'FreshCart')
+        } 
+      })
+    }
+  
+    isInWishlist(productId: string): boolean {
+      return this.wishlistIds.has(productId);
+    }
+  
+    addToWishlist(p_id:string){
+      if (this.isInWishlist(p_id)) {
+        this.removeSubID = this._WishlistService.removeProductFromWishlist(p_id).subscribe({
+          next: () => {
+            this.wishlistIds.delete(p_id);
+            this._ToastrService.warning('Removed from Wishlist', 'FreshCart');
+          }
+        });
+      } else {
+        this.addListSubID = this._WishlistService.addProductToWishlist(p_id).subscribe({
+          next: (res) => {
+            this.wishlistIds.add(p_id);
+            this._ToastrService.success('Added to Wishlist', 'FreshCart');
+          }
+        });
+      }
+    }
+  
+    ngOnDestroy(): void {
+      this.categorySub?.unsubscribe();
+      this.productSub?.unsubscribe();
+      this.addCartSubID?.unsubscribe();
+      this.addListSubID?.unsubscribe();
+      this.loggedUserSubID?.unsubscribe();
+      this.removeSubID?.unsubscribe();
+    }
 }
